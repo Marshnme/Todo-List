00dc406f1a5b3517d7fbf15c256b54ec4fcdcf48
Marshnme <jildsmiles@yahoo.com>
2022-06-06 15:53:26 -0400

    basic guidlines for project in comments

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// todo
+    // single page app
+    // home screen will have inputs for adding todos and projects that will hold todos. Choose which project your todo goes into
+    // Factories to create todos and projects
+    // todos will have title,desc,duedate,priority.....etc
+    // Keep logic seperated and Dom related stuff seperate.
+    // interface should be able to view all projects,view all todays in each project,expand a single todo to see/edit its details,and delete a todo.
+    // checkout date-fns package 
+    // use local storage to keep the session of the user
