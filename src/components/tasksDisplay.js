1e4bbfebb4d16cba1f18fb523b7bb1c5b188fdc7
Marshnme <jildsmiles@yahoo.com>
2022-06-08 12:28:42 -0400

    removed duplicate code

--- src/components/tasksDisplay.js
+++ src/components/tasksDisplay.js
@@ -4,7 +4,7 @@ import "./tasks.css"
 import taskUtility from "./taskUtilityBar"
 
 const tasksDisplay = () => {
-    let {toggleComplete,refreshTaskList} = manageTodos()
+    let {refreshTaskList} = manageTodos()
     taskUtility()
 
     const contentDiv = document.querySelector(".content")
@@ -15,23 +15,6 @@ const tasksDisplay = () => {
 
     refreshTaskList()
 
-// //    using this exact code in my todo refreshtask func
-//     todos.map(todo => {
-//         let todoItem = document.createElement("div")
-//         let todoTitle = document.createElement("h2")
-//         let todoDueBy = document.createElement("p")
-//         todoItem.classList.add(`todo`,`todo-${todo.id}`)
-//         todoTitle.textContent=`${todo.title}`
-//         todoDueBy.textContent=`${todo.dueBy}`
-//         todoItem.addEventListener("click",toggleComplete)
-
-//         todoContainer.appendChild(todoItem)
-//         todoItem.appendChild(todoTitle)
-//         todoItem.appendChild(todoDueBy)
-        
-    
-//     })
-//     contentDiv.appendChild(todoContainer)
 }
 
 export default tasksDisplay
