36af9854632795ed9cb720a14617e6831352f51f
Marshnme <jildsmiles@yahoo.com>
2022-06-08 15:59:13 -0400

    uncommented code that saved task state after updating complete status

--- src/logic/todos.js
+++ src/logic/todos.js
@@ -71,7 +71,7 @@ function manageTodos(){
             }else{
                 console.log("nowork")
             }
-            // saveTasksInLocalStorage()
+            saveTasksInLocalStorage()
         }
         console.log(todos)
         
