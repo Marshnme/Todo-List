498a3d3e3994d18405ca92dced1533df78b0ee2e
Marshnme <jildsmiles@yahoo.com>
2022-06-09 20:23:15 -0400

    converting logic from tasks to projects

--- src/components/projectsDisplay.js
+++ src/components/projectsDisplay.js
@@ -1,15 +1,19 @@
-
+import {manageProjects} from "../logic/projects"
 
 const projectsDisplay = () => {
-
+    let {refreshProjects} = manageProjects()
     const contentDiv = document.querySelector(".content")
 
     let projectContainer = document.createElement("div")
     projectContainer.classList.add("project-container")
+
     let projectTitle = document.createElement("p")
     projectTitle.textContent = "WOOORK"
+
     projectContainer.appendChild(projectTitle)
     contentDiv.appendChild(projectContainer)
+
+    refreshProjects()
 }
 
 export default projectsDisplay
--- src/logic/projects.js
+++ src/logic/projects.js
@@ -1,15 +1,58 @@
+ let projects = []
 
- function Project(title,desc,dueBy,priority,complete){
-    title
-    desc
-    dueBy
-    priority
-    complete
+ function manageProjects(){
 
-    let projects = []
     let addProject = function(){
-        projects.push(this)
+        projects = [...projects, this]
+        console.log(projects)
     }
+
+    let refreshProjectList = function(){
+        if(!projects){
+            return
+        }
+
+        const contentDiv = document.querySelector(".content")
+        let projectContainer = document.querySelector(".project-container")
+        projectContainer.replaceChildren()
+
+        projects.map(project => {
+            let projectDetails = document.createElement("div")
+            projectDetails.classList.add("project-details")
+            let projectItem = document.createElement("div")  
+            let projectTitle = document.createElement("h2")
+            let projectDueBy = document.createElement("p")
+            let projectDelete = document.createElement("p")
+            projectDelete.classList.add("delete-icon")
+
+            projectItem.classList.add(`project`,`project-`)
+
+            if(project.complete === true || project.complete === "true"){
+                console.log("IM COMPLETE")
+                projectItem.classList.add("project-completed-background")
+                projectDetails.classList.add("project-completed")
+            }else{
+                console.log("i am not completed")
+                projectItem.classList.remove("project-completed-background")
+                projectDetails.classList.remove("project-completed")
+            }
+
+            projectTitle.textContent=`${project.title}`
+            projectDueBy.textContent=`${project.dueBy}`
+            projectDelete.textContent="DELETE"
+
+            // projectItem.addEventListener("click",toggleComplete)
+            // projectDelete.addEventListener("click",deleteProject)
+    
+            projectContainer.appendChild(projectItem)
+            projectItem.appendChild(projectDetails)
+            projectDetails.appendChild(projectTitle)
+            projectDetails.appendChild(projectDueBy)
+            projectItem.appendChild(projectDelete)
+        })
+        contentDiv.appendChild(todoContainer)
+    }
+
     let toggleComplete = function(){
         this.classList.toggle('complete')
     }
@@ -29,7 +72,18 @@
             // projects = updatedProjects;
         }
     }
-    return {addProject,toggleComplete,deleteProject}
+    return {addProject,refreshProjectList,toggleComplete,deleteProject}
+ }
+ function Project(title,desc,dueBy,priority,complete){
+    title
+    desc
+    dueBy
+    priority
+    complete
+
+    let {addProject} = manageProjects()
+
+    return { title,desc,dueBy,priority,complete,addProject}
 }
 
-export default Project
+export {Project, manageProjects,projects}
